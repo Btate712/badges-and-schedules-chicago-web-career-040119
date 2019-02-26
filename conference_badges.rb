0ae01f01197548b567f07efc40b0bd334738c860
Robert Tate <btate712@gmail.com>
2019-02-26 17:12:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  return_array = []
+  attendees.each do |attendee|
+    return_array.push(badge_maker(attendee))
+  end
+  return_array
+end
