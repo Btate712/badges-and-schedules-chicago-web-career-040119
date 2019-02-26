2f387d07b799b8f4145857e94f07872f4ff42ff8
Robert Tate <btate712@gmail.com>
2019-02-26 17:17:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ def assign_rooms(attendees)
   next_room_number = 1
   return_array = []
   attendees.each do |attendee|
-    return_array.push("Hello, #{attendee}! You'll be assigned to room #{next_room_number}")
+    return_array.push("Hello, #{attendee}! You'll be assigned to room #{next_room_number}!")
     next_room_number += 1
   end
   return_array
